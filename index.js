e5c737eca72e2ea45c123a5237bbe00d6d237a06
leito <leogturzi@gmail.com>
2022-06-26 16:39:40 -0300

    people endpoint created

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const fetchSwapi = require("./fetchSwapi");
 
 // instantiate express
 const app = express();
+
+app.get("/api/people", async (req, res) => {
+  const swapiData = await fetchSwapi();
+  res.send(swapiData);
+});
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
